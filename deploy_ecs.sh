fd27095297bcd7f846ea0a7fa4042d41d7df4af1
disc99 <disc99.g@gmail.com>
2017-11-09 11:21:36 +0900

    Fix CI error

--- deploy_ecs.sh
+++ deploy_ecs.sh
@@ -12,17 +12,16 @@ REPOSITORY=${ECR}/${SERVICE}
 login="$(aws ecr get-login --region ap-northeast-1)"
 ${login}
 
-
 # for backup
 docker tag ${SERVICE} ${REPOSITORY}:${BUILD_ID}
 docker push ${REPOSITORY}:${BUILD_ID}
 
 # for deploy
-docker tag ${IMAGE_NAME} ${REPOSITORY}:latest
+docker tag ${SERVICE} ${REPOSITORY}:latest
 docker push ${REPOSITORY}:latest
 
 # update task definition
 aws ecs register-task-definition --cli-input-json file://${TASK_DEFINITION_PATH}/${SERVICE}.json --region "ap-northeast-1"
 
 # update service
-aws ecs update-service --service ${SERVICE} --task-definition ${SERVICE} --cluster ${CLUSTER_NAME} --region "ap-northeast-1"
+aws ecs update-service --service ${SERVICE} --task-definition ${SERVICE} --cluster ${CLUSTER} --region "ap-northeast-1"
