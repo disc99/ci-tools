778a6bb31c561cf15deba7670baebda83fa40b6f
disc99 <disc99.g@gmail.com>
2017-11-09 12:07:36 +0900

    Update deploy shell

--- deploy_ecs.sh
+++ deploy_ecs.sh
@@ -7,9 +7,40 @@ TASK_DEFINITION_PATH=$4
 BUILD_ID=$5
 
 REPOSITORY=${ECR}/${SERVICE}
+TIMEOUT=600
+REGION=ap-northeast-1
+
+# wait function
+function waitForServiceUpdate {
+  DEPLOYMENT_SUCCESS="false"
+  every=2
+  i=0
+  echo "Waiting for service deployment to complete..."
+  while [ $i -lt $TIMEOUT ]
+  do
+    NUM_DEPLOYMENTS=$(aws ecs describe-services --services $SERVICE --cluster $CLUSTER --region ${REGION} | jq "[.services[].deployments[]] | length")
+
+    # Wait to see if more than 1 deployment stays running
+    # If the wait time has passed, we need to roll back
+    if [ "$NUM_DEPLOYMENTS" = "1" ]; then
+      echo "Service deployment successful."
+      DEPLOYMENT_SUCCESS="true"
+      # Exit the loop.
+      i=$TIMEOUT
+    else
+      sleep $every
+      i=$(( $i + $every ))
+    fi
+  done
+
+  if [[ "${DEPLOYMENT_SUCCESS}" != "true" ]]; then
+    exit 1
+  fi
+}
+
 
 # login ECR
-login="$(aws ecr get-login --region ap-northeast-1)"
+login="$(aws ecr get-login --region ${REGION})"
 ${login}
 
 # for backup
@@ -21,7 +52,10 @@ docker tag ${SERVICE} ${REPOSITORY}:latest
 docker push ${REPOSITORY}:latest
 
 # update task definition
-aws ecs register-task-definition --cli-input-json file://${TASK_DEFINITION_PATH}/${SERVICE}.json --region "ap-northeast-1"
+aws ecs register-task-definition --cli-input-json file://${TASK_DEFINITION_PATH}/${SERVICE}.json --region ${REGION}
 
 # update service
-aws ecs update-service --service ${SERVICE} --task-definition ${SERVICE} --cluster ${CLUSTER} --region "ap-northeast-1"
+aws ecs update-service --service ${SERVICE} --task-definition ${SERVICE} --cluster ${CLUSTER} --region ${REGION}
+
+# wait
+waitForServiceUpdate
